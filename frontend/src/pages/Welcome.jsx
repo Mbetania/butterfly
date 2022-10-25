a2f8b47b4a40274bd8b0f21e84b9c66518e70657
betaniagonzalez31@gmail.com <betaniagonzalez31@gmail.com>
2022-10-24 23:00:41 -0300

    update

---
+++ frontend/src/pages/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Welcome = () => {
+return (
+    <main id='welcome-container' className='d-flex flex-column p-4 p-xxl-5 justify-content-around align-items-center'>
+        <section>
+            <h1></h1>
+            <ul>
+                <li>Ingresa tus credenciales</li>
+                <li>Configura tus locaciones</li>
+                <li>Configura las tarifas</li>
+            </ul>
+        </section>
+    </main>
+)
+}
---
+++ frontend/src/routes/IndexRoutes.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { Login } from '../pages/onboarding/Login';
+import { NavOnboarding } from '../pages/onboarding/NavOnboarding';
+import { Welcome } from '../pages/Welcome';
+
+
+const clientRoutes = 'butterfly'
+
+export const IndexRoutes = () => {
+  
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={`/${clientRoutes}/`}>
+          <Route path='welcome' element={<Welcome client={clientRoutes} />} />
+          <Route path='onboarding' element={<NavOnboarding client={clientRoutes}/>}> 
+            <Route path='login' element={<Login/>} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
